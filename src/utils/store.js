5562ca8a66bfccec3da5203173db267b8ce0e9c3
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2022-01-24 17:18:38 +0530

    Initial commit

---
+++ src/utils/store.js
@@ -0,0 +1,53 @@
+// const data = {
+//     lists: {
+//       "list-1": {
+//         id: "list-1",
+//         title: "To do",
+//         cards: [
+//           {
+//             id: "1",
+//             title: "vim practice",
+//           },
+//         ],
+//       },
+//       "list-2": {
+//         id: "list-2",
+//         title: "In Progress",
+//         cards: [],
+//       },
+//       "list-3": {
+//         id: "list-3",
+//         title: "Completed",
+//         cards: [],
+//       },
+//     },
+//     listIds: ["list-1", "list-2", "list-3"],
+//   };
+
+const data = {
+  lists: [
+    {
+      id: "list-1",
+      title: "To do",
+      cards: [
+        {
+          id: "1",
+          title: "vim practice",
+        },
+      ],
+    },
+    {
+      id: "list-2",
+      title: "In Progress",
+      cards: [],
+    },
+    {
+      id: "list-3",
+      title: "Completed",
+      cards: [],
+    },
+  ],
+  listIds: ["list-1", "list-2", "list-3"],
+};
+
+export default data;
